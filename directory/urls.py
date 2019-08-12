1f6c27972f7c4102141e2fcf2ee1a3641df092ba
david.raskin8 <david.raskin8@gmail.com>
2019-08-12 10:51:25 -0500

    built adding folder functionality

--- directory/urls.py
+++ directory/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
-        path('get_sub_dir', views.get_sub_dir, name='get_sub_dir')
+        path('get_sub_dir', views.get_sub_dir, name='get_sub_dir'),
+        path('add_folder', views.add_folder, name='add_folder')
 ]
--- directory/views.py
+++ directory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from directory.models import Folder, File
 from .forms import AddFolderForm
 import json
@@ -12,8 +12,8 @@ def index(request):
         form = AddFolderForm()
         context['folder_form'] = form.as_p()
 
-    if request.method == 'POST':
-        print('blar')
+    # if request.method == 'POST':
+    #     form = AddFolderForm(request.POST)
 
     return render(request, 'base_generic.html', context=context)
 
@@ -22,18 +22,20 @@ def get_sub_dir(request):
 
     return HttpResponse(json.dumps(context))
 
-# def add_folder(request):
-#     if request.method == 'POST':
-#         form = AddFolderForm(request.POST)
+def add_folder(request):
+    if request.method == 'POST':
+        form = AddFolderForm(request.POST)
+        if form.is_valid():
+            folder_name = form.cleaned_data['folder_name']
+            parent_id = form.cleaned_data['parent_id']
 
-#         if form.is_valid():
+            parent_folder = Folder.objects.get(id=parent_id)
+            full_path = parent_folder.full_path + '/' + folder_name
+            new_folder = Folder(name=folder_name, parent=parent_folder, owner='davidraskin8', full_path=full_path)
+            new_folder.save()
 
-#             return HttpResponseRedirect('/index/')
-
-#     else:
-#         form = AddFolderForm()
-
-#     return render(request, 'base_generic.html', {'folder_form': form})
+            return HttpResponseRedirect('/')
+        
 
 # Internal function used to retrieve the files and folders in a folder of id=fid
 def retrieve_sub_dir(fid):
